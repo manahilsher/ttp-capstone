c4d5343b6c282266c1e734d1c33459b9432aaaf7
Manahil Sher <manahil.sher@gmail.com>
2020-06-15 17:12:40 -0400

    added user export to models index

---
+++ database/models/index.js
@@ -0,0 +1,7 @@
+// Here, we can prepare to register our models, set up associations between tables, and generate a barrel file for the models;
+
+const User = require('./user');
+
+module.exports = {
+  User
+};
